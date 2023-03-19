57cee9ad6cc1d70954ca4e82d172a59b05974952
Elliotico <48328072+Elliotico@users.noreply.github.com>
2023-03-19 18:53:54 -0300

    adcionado js e mais

---
+++ Js/script.js
@@ -0,0 +1,16 @@
+const FRASE_ALEATORIA_API_URL = 'https://random-word-api.herokuapp.com/word'
+const fraseDisplayElement = document.getElementById('fraseAleatoria')
+
+function getFraseAleatoria() {
+    return fetch(FRASE_ALEATORIA_API_URL)
+        .then(Response => Response.json())
+        .then(data => data.content)
+}
+
+async function renderProximaFrase() {
+    const word = getFraseAleatoria()
+    console.log(word) 
+    fraseDisplayElement.innerText = word
+}
+
+renderProximaFrase()
