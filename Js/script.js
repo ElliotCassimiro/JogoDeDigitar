6e5c3734d6ee6a129e7dc02ea85aa6791e383467
Elliotico <48328072+Elliotico@users.noreply.github.com>
2023-03-20 08:16:24 -0300

    Finalizando

--- Js/script.js
+++ Js/script.js
@@ -1,15 +1,61 @@
-const FRASE_ALEATORIA_API_URL = 'https://random-word-api.herokuapp.com/word'
-const fraseDisplayElement = document.getElementById('fraseAleatoria')
+const FRASE_ALEATORIA_API_URL = 'http://api.quotable.io/random'
+const quoteDisplayElement = document.getElementById('quoteDisplay')
+const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
 
-function getFraseAleatoria() {
-    return fetch(FRASE_ALEATORIA_API_URL)
-        .then(Response => Response.json())
-        .then(data => data.content)
+quoteInputElement.addEventListener('input', () => {
+  const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+  const arrayValue = quoteInputElement.value.split('')
+
+  let correct = true
+  arrayQuote.forEach((characterSpan, index) => {
+    const character = arrayValue[index]
+    if (character == null) {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.remove('incorrect')
+      correct = false
+    } else if (character === characterSpan.innerText) {
+      characterSpan.classList.add('correct')
+      characterSpan.classList.remove('incorrect')
+    } else {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.add('incorrect')
+      correct = false
+    }
+  })
+
+  if (correct) renderNewQuote()
+})
+
+function getRandomQuote() {
+  return fetch(FRASE_ALEATORIA_API_URL)
+    .then(response => response.json())
+    .then(data => data.content)
 }
 
-async function renderProximaFrase() {
-    const word = getFraseAleatoria()
-    fraseDisplayElement.innerText = word
+async function renderNewQuote() {
+  const quote = await getRandomQuote()
+  quoteDisplayElement.innerHTML = ''
+  quote.split('').forEach(character => {
+    const characterSpan = document.createElement('span')
+    characterSpan.innerText = character
+    quoteDisplayElement.appendChild(characterSpan)
+  })
+  quoteInputElement.value = null
+  startTimer()
 }
 
-renderProximaFrase()
+let startTime
+function startTimer() {
+  timerElement.innerText = 0
+  startTime = new Date()
+  setInterval(() => {
+    timer.innerText = getTimerTime()
+  }, 1000)
+}
+
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000)
+}
+
+renderNewQuote()
